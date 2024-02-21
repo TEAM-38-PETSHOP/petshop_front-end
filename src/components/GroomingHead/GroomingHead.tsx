989afb09b9ec934bf5f498a6e6d03712d23df1c0
Mykola Maik <mykoluamaik@gmail.com>
2024-02-21 21:49:19 +0200

    Create-grooming-head-component

---
+++ src/components/GroomingHead/GroomingHead.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import Arrow from '../Arrow/Arrow';
+import style from './GroomingHead.module.scss';
+import dog from '@@/images/grooming/small-dog.svg?url';
+import cat from '@@/images/grooming/small-cat.svg?url';
+import Image from 'next/image';
+
+interface Props {
+  pet: string;
+}
+
+export default function GroomingHead({ pet }: Props) {
+  const to = pet === 'dogs' ? 'cats' : 'dogs';
+  const currentPet = pet === 'dogs' ? 'песиків' : 'котиків';
+  
+  return (
+    <section className={style.groomingHead}>
+      <Link className={style.groomingHead__goBack} href="/grooming">
+        <Arrow 
+          styleName={style.groomingHead__goBack__arrow}
+          direction='left'
+        />
+      </Link>
+      <div className={style.groomingHead__centerContainer}>
+        <Image
+          className={style.groomingHead__centerContainer__img}
+          src={pet === 'dogs' ? dog : cat}
+          alt='dog'
+        />
+        <h2 className={style.groomingHead__centerContainer__title}>
+          Грумінг для {currentPet}
+        </h2>
+      </div>
+      <Link className={style.groomingHead__move} href={`/grooming/${to}`}>
+        Грумінг для {pet === 'dogs' ? 'котиків' : 'песиків'}
+      </Link>
+    </section>
+  );
+}
