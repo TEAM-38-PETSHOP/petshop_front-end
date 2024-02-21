1646302a7e539b4dbf9170e8bc2179425d7bb623
Mykola Maik <mykoluamaik@gmail.com>
2024-02-21 15:26:51 +0200

    Create-goal-component

--- src/app/grooming/[pet]/page.tsx
+++ src/app/grooming/[pet]/page.tsx
@@ -1,3 +1,8 @@
+import Goal from "@/components/Goal/Goal";
+
 export default function GroomingPet() {
-  return <div>Grooming Pet</div>;
+  return <>
+    <h1 style={{ textAlign: "center", marginBottom: "50px" }}>Grooming</h1>
+    <Goal />
+  </>;
 }
