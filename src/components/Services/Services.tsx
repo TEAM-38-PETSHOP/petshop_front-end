445fcfd0ebc5445f9827bd15d2930b9bc72283d7
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-03-01 17:40:22 +0200

    Created Services component

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,20 +1,72 @@
 import Image from 'next/image';
 import styles from './services.module.scss';
 import ButtonWithArrow from '../ButtonWithArrow/ButtonWithArrow';
+import classNames from 'classnames';
 export default function Services() {
   return (
     <section className={styles.services}>
       <h2 className={styles.services__title}>Кого потрібно причепурити?</h2>
 
-      <div className={styles.services__blocks}></div>
-      <div className={styles.services__block}></div>
-      <div className={styles.services__imgBlock}>
-        <ButtonWithArrow
-          text="Доглянути котика"
-          variant="orange"
-        />
+      <div className={styles.services__blocks}>
+        <div
+          className={classNames(
+            [styles.services__firstBlock],
+            [styles.services__img]
+          )}
+        >
+          <ButtonWithArrow
+            classNameBtn={styles.services__buttons}
+            text="Доглянути котика"
+            href="/grooming/cats"
+            variant="green"
+          />
+        </div>
+        <div
+          className={classNames(
+            [styles.services__description],
+            [styles.services__firstBlock_description]
+          )}
+        >
+          <h3>Grooming Spa</h3>
+          <p>
+            Наші найсучасніші засоби догляду розроблені, щоб створити приємні
+            враження без стресу
+          </p>
+        </div>
+        <div
+          className={classNames(
+            [styles.services__imgBlock],
+            [styles.services__img]
+          )}
+        >
+          <ButtonWithArrow
+            classNameBtn={styles.services__buttons}
+            text="Доглянути котика"
+            href="/grooming/dogs"
+            variant="orange"
+          />
+        </div>
+        <div
+          className={classNames(
+            [styles.services__lastBlock],
+            [styles.services__img]
+          )}
+        >
+          <ButtonWithArrow
+            classNameBtn={styles.services__buttons}
+            href="/catalog"
+            text="До магазину"
+          />
+        </div>
+        <div
+          className={classNames(
+            [styles.services__description],
+            [styles.services__lastBlock_description]
+          )}
+        >
+          <p>Послуги та ціни </p>
+        </div>
       </div>
-      <div className={styles.services__block}></div>
     </section>
   );
 }
