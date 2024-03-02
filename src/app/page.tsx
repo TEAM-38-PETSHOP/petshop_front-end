d828442b9d9ebd42a4b7fd256645b44ec53f4a77
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-03-02 22:02:48 +0200

    Connecting Services to RoadToUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import About from '@/components/About/About';
 import MainBenefits from '@/components/MainBenefits/MainBenefits';
 import ProductSlider from '@/components/ProductSlider/ProductSlider';
-import RoadToUs from '@/components/RoadToUs/RoadToUs';
 import Services from '@/components/Services/Services';
+import RoadToUs from '@/components/RoadToUs/RoadToUs';
 
 export default function Home() {
   return (
