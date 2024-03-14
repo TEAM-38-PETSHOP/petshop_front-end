37b198e7362f7f53b0bbcc2888a5be20ca0291ad
Mykola Maik <mykoluamaik@gmail.com>
2024-03-14 21:33:49 +0200

    fix-vercel-error-wrapped-components-inside-suspense

--- src/app/grooming/[pet]/page.tsx
+++ src/app/grooming/[pet]/page.tsx
@@ -1,6 +1,7 @@
 import Goal from "@/components/Goal/Goal";
 import GroomingForm from "@/components/GroomingForm/GroomingForm";
 import GroomingHead from "@/components/GroomingHead/GroomingHead";
+import { Suspense } from "react";
 
 type Props = {
   params: {
@@ -11,8 +12,14 @@ type Props = {
 export default function GroomingPet({ params: { pet } }: Props) {
   return (
     <>
-      <GroomingHead pet={pet} />
-      <GroomingForm pet={pet} />
+      <Suspense>
+        <GroomingHead pet={pet} />
+      </Suspense>
+
+      <Suspense>
+        <GroomingForm pet={pet} />
+      </Suspense>
+      
       <Goal />
     </>
   );
