87f87894dbad9a76dce01359a64fd993bf280f58
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-04-02 13:22:21 +0300

    Fix server link

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://ec2-23-22-30-240.compute-1.amazonaws.com';
+const BASE_URL = 'http://ec2-54-147-134-232.compute-1.amazonaws.com';
 
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
