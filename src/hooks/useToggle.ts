1832487beb10cb526851b866c646f35653493d6c
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-04-17 17:10:05 +0300

    Update useToggle

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,7 @@
 import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
 import { Product } from '../types/Product';
 import { useAppDispatch } from './reduxHooks';
+import { useEffect, useState } from 'react';
 
 export const useToggle = (
   nameProd: string,
@@ -9,9 +10,13 @@ export const useToggle = (
   carrProduct: Product
 ): [boolean, () => void] => {
   const dispatch = useAppDispatch();
-  const isSelected = products
-    ? products.some((prod) => prod.productId === carrProduct.productId)
-    : false;
+  const [isSelected, setIsSelected] = useState(false);
+
+  useEffect(() => {
+    setIsSelected(
+      products.some((prod) => prod.productId === carrProduct.productId)
+    );
+  }, [carrProduct.productId, products]);
 
   const toggler = () => {
     if (products) {
