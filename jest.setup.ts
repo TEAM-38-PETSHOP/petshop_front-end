eebe3a8a5c403c3a5d182aaaac9ffd50d5b7a8e7
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-04-25 14:00:45 +0300

    Moved mock next/navigation to jest.setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,20 @@
 import '@testing-library/jest-dom';
 
+jest.mock('next/navigation', () => {
+  return {
+    __esModule: true,
+    useRouter: () => ({
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    }),
+    useSearchParams: () => ({
+      get: () => {},
+    }),
+    usePathname: () => '/',
+  };
+});
+
 global.matchMedia =
   global.matchMedia ||
   function () {
