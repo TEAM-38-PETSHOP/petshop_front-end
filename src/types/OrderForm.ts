abf34ea6d4954ae579eb9e2ee25ec1f4e910d044
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-06-17 17:22:14 +0300

    Created api for novaposhta and add types

---
+++ src/types/OrderForm.ts
@@ -0,0 +1,10 @@
+export interface IOrderForm {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  city: string;
+  deliveryMethod: string;
+  deliveryPoint: string;
+  paymentMethod: string;
+  comment?: string;
+}
