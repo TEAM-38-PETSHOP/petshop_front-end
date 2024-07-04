7de2b266d670bdfbbafc8066f336488880773c21
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-07-04 19:59:14 +0300

    Added PreviewOrder component

--- src/app/cart/checkout/page.tsx
+++ src/app/cart/checkout/page.tsx
@@ -1,13 +1,15 @@
 import HeaderForPages from '@/components/HeaderForPages/HeaderForPages';
 import styles from './checkout.module.scss';
 import OrderForm from '@/components/ForCheckout/OrderForm/OrderForm';
+import PreviewOrder from '@/components/ForCheckout/PreviewOrder/PreviewOrder';
 export default function Checkout() {
   return (
     <section className={styles.checkout}>
       <HeaderForPages centralBlock={{ text: 'Оформлення замовлення' }} />
 
       <div className={styles.checkout__wrapper}>
-        <OrderForm />
+        <OrderForm className={styles.checkout__form} />
+        <PreviewOrder className={styles.checkout__preview} />
       </div>
     </section>
   );
