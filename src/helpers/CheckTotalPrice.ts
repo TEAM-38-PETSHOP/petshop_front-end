c05af0ac29c4afdcd8af20691a1cf211f1564c29
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-07-09 12:13:05 +0300

    Fixed types in CheckTotalPrice

--- src/helpers/CheckTotalPrice.ts
+++ src/helpers/CheckTotalPrice.ts
@@ -4,15 +4,21 @@ export const CheckTotalPrice = () => {
   const cartProducts =
     checkWindow() && JSON.parse(localStorage.getItem('cart') || '[]');
 
-  const newTotalPrice = cartProducts.reduce((acc, item) => {
-    const quantity = checkWindow()
-      ? +(localStorage.getItem(item.productId.toString()) || 1)
-      : 1;
+  const newTotalPrice = cartProducts.reduce(
+    (
+      acc: number,
+      item: { productId: { toString: () => string }; price: number }
+    ) => {
+      const quantity = checkWindow()
+        ? +(localStorage.getItem(item.productId.toString()) || 1)
+        : 1;
 
-    const sum = item.price * quantity;
+      const sum = item.price * quantity;
 
-    return acc + sum;
-  }, 0);
+      return acc + sum;
+    },
+    0
+  );
 
   return newTotalPrice;
 };
