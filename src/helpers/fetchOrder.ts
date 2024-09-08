6ca1f5ebc020d487221cc883527f257a4b50c7de
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-09-08 19:50:55 +0300

    Added order creation for authorized user and improved order types

--- src/helpers/fetchOrder.ts
+++ src/helpers/fetchOrder.ts
@@ -1,12 +1,12 @@
-import {
-  IOrderWithoutAuth,
-  IOrderWithoutAuthResponse,
-} from '@/types/OrderWithoutAuth';
+import { IOrderResponse } from '@/types/OrderResponse';
+import { IOrderWithAuth } from '@/types/OrderWithAuth';
+import { IOrderWithoutAuth } from '@/types/OrderWithoutAuth';
 import { client } from '@/utils/fetchClient';
 
-export const createOrder = (data: IOrderWithoutAuth) => {
-  return client.post<IOrderWithoutAuthResponse>(
-    `/api/orders/withoutAuth`,
-    data
-  );
+export const createOrderWithoutAuth = (data: IOrderWithoutAuth) => {
+  return client.post<IOrderResponse>(`/api/orders/withoutAuth`, data);
+};
+
+export const createOrderWithAuth = (data: IOrderWithAuth, token: string) => {
+  return client.post<IOrderResponse>(`/api/orders`, data, token);
 };
